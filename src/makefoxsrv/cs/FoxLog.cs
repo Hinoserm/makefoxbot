510898e7ee1cef088fb387445c276b643cb69ebf
Hinoserm <hinoserm@protomuck.org>
2024-05-19 17:48:04 -0500

    Adjustment to logging function.

--- src/makefoxsrv/cs/FoxLog.cs
+++ src/makefoxsrv/cs/FoxLog.cs
@@ -31,36 +31,39 @@ namespace makefoxsrv
         // Logging function
         public static void Write(string message, LogLevel level = LogLevel.INFO, [CallerMemberName] string callerName = "", [CallerFilePath] string callerFilePath = "", [CallerLineNumber] int lineNumber = 0)
         {
-            try
+            _= Task.Run(() =>
             {
-                string dateFormat = "dd MMM yyyy hh:mm:ss.ff tt";
-
-                string[] lines = Regex.Split(message, @"\r\n|\r|\n");
-
-                // Check if the last item is an empty string and remove it if necessary
-                if (lines.Length > 1 && lines[^1] == "")
+                try
                 {
-                    Array.Resize(ref lines, lines.Length - 1);
+                    string dateFormat = "dd MMM yyyy hh:mm:ss.ff tt";
+
+                    string[] lines = Regex.Split(message, @"\r\n|\r|\n");
+
+                    // Check if the last item is an empty string and remove it if necessary
+                    if (lines.Length > 1 && lines[^1] == "")
+                    {
+                        Array.Resize(ref lines, lines.Length - 1);
+                    }
+
+                    lock (fileLock) // Ensures one thread writes to the file at a time.
+                    {
+                        File.AppendAllLines("../logs/output.txt", lines.Select(line => $"{DateTime.Now.ToString(dateFormat)} {level}> {line}").ToArray());
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Error writing log: {e.Message}");
                 }
 
-                lock (fileLock) // Ensures one thread writes to the file at a time.
+                // Check if the current log message level is greater than or equal to the application's log level
+                if (level >= CurrentLogLevel)
                 {
-                    File.AppendAllLines("../logs/output.txt", lines.Select(line => $"{DateTime.Now.ToString(dateFormat)} {level}> {line}").ToArray());
+                    // Log the message with a timestamp and the level
+                    //FoxLog.WriteLine();
+                    //FoxUI.AppendLog($"{DateTime.Now} [{level}]: {message}");
+                    FoxUI.AppendLog(message);
                 }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine($"Error writing log: {e.Message}");
-            }
-
-            // Check if the current log message level is greater than or equal to the application's log level
-            if (level >= CurrentLogLevel)
-            {
-                // Log the message with a timestamp and the level
-                //FoxLog.WriteLine();
-                //FoxUI.AppendLog($"{DateTime.Now} [{level}]: {message}");
-                FoxUI.AppendLog(message);
-            }
+            });
         }
     }
 }
