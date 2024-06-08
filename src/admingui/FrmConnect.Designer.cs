a81dc2fd3069094515f80e6eea01664d15d829b2
Hinoserm <hinoserm@protomuck.org>
2024-06-08 16:04:02 -0500

    Oops.  Adding actual GUI project.

---
+++ src/admingui/FrmConnect.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace admingui
+{
+    partial class frmConnect
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            cmbServerAddress = new ComboBox();
+            label1 = new Label();
+            label2 = new Label();
+            txtUsername = new TextBox();
+            label3 = new Label();
+            txtPassword = new TextBox();
+            chkRememberPassword = new CheckBox();
+            btnConnect = new Button();
+            btnCancel = new Button();
+            SuspendLayout();
+            // 
+            // cmbServerAddress
+            // 
+            cmbServerAddress.FormattingEnabled = true;
+            cmbServerAddress.Items.AddRange(new object[] { "http://makefox.bot/cs/ws", "http://localhost:5555/ws" });
+            cmbServerAddress.Location = new Point(12, 33);
+            cmbServerAddress.Name = "cmbServerAddress";
+            cmbServerAddress.Size = new Size(312, 23);
+            cmbServerAddress.TabIndex = 0;
+            cmbServerAddress.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 15);
+            label1.Name = "label1";
+            label1.Size = new Size(87, 15);
+            label1.TabIndex = 1;
+            label1.Text = "Server Address:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(9, 59);
+            label2.Name = "label2";
+            label2.Size = new Size(63, 15);
+            label2.TabIndex = 2;
+            label2.Text = "Username:";
+            label2.Click += label2_Click;
+            // 
+            // txtUsername
+            // 
+            txtUsername.Location = new Point(12, 77);
+            txtUsername.Name = "txtUsername";
+            txtUsername.Size = new Size(160, 23);
+            txtUsername.TabIndex = 3;
+            txtUsername.TextChanged += txtUsername_TextChanged;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(12, 103);
+            label3.Name = "label3";
+            label3.Size = new Size(60, 15);
+            label3.TabIndex = 4;
+            label3.Text = "Password:";
+            label3.Click += label3_Click;
+            // 
+            // txtPassword
+            // 
+            txtPassword.Location = new Point(12, 121);
+            txtPassword.Name = "txtPassword";
+            txtPassword.Size = new Size(160, 23);
+            txtPassword.TabIndex = 5;
+            txtPassword.TextChanged += textBox2_TextChanged;
+            // 
+            // chkRememberPassword
+            // 
+            chkRememberPassword.AutoSize = true;
+            chkRememberPassword.Checked = true;
+            chkRememberPassword.CheckState = CheckState.Checked;
+            chkRememberPassword.Location = new Point(12, 150);
+            chkRememberPassword.Name = "chkRememberPassword";
+            chkRememberPassword.Size = new Size(137, 19);
+            chkRememberPassword.TabIndex = 6;
+            chkRememberPassword.Text = "Remember Password";
+            chkRememberPassword.UseVisualStyleBackColor = true;
+            chkRememberPassword.CheckedChanged += checkBox1_CheckedChanged;
+            // 
+            // btnConnect
+            // 
+            btnConnect.Location = new Point(249, 176);
+            btnConnect.Name = "btnConnect";
+            btnConnect.Size = new Size(75, 23);
+            btnConnect.TabIndex = 7;
+            btnConnect.Text = "Connect";
+            btnConnect.UseVisualStyleBackColor = true;
+            btnConnect.Click += btnConnect_Click;
+            // 
+            // btnCancel
+            // 
+            btnCancel.Location = new Point(168, 176);
+            btnCancel.Name = "btnCancel";
+            btnCancel.Size = new Size(75, 23);
+            btnCancel.TabIndex = 8;
+            btnCancel.Text = "Cancel";
+            btnCancel.UseVisualStyleBackColor = true;
+            btnCancel.Click += btnCancel_Click;
+            // 
+            // frmConnect
+            // 
+            AcceptButton = btnConnect;
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            CancelButton = btnCancel;
+            ClientSize = new Size(336, 213);
+            Controls.Add(btnCancel);
+            Controls.Add(btnConnect);
+            Controls.Add(chkRememberPassword);
+            Controls.Add(txtPassword);
+            Controls.Add(label3);
+            Controls.Add(txtUsername);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(cmbServerAddress);
+            FormBorderStyle = FormBorderStyle.FixedDialog;
+            Name = "frmConnect";
+            Text = "MakeFox Manager - Connect";
+            Load += FrmConnect_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ComboBox cmbServerAddress;
+        private Label label1;
+        private Label label2;
+        private TextBox txtUsername;
+        private Label label3;
+        private TextBox txtPassword;
+        private CheckBox chkRememberPassword;
+        private Button btnConnect;
+        private Button btnCancel;
+    }
+}
---
+++ src/admingui/FrmConnect.cs
@@ -0,0 +1,130 @@
+using Microsoft.Win32;
+
+namespace admingui
+{
+    public partial class frmConnect : Form
+    {
+        public frmConnect()
+        {
+            InitializeComponent();
+        }
+
+        private const string RegistryKeyPath = @"SOFTWARE\MakeFoxManager";
+
+        private void FrmConnect_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                RegistryKey key = Registry.CurrentUser.OpenSubKey(RegistryKeyPath);
+                if (key != null)
+                {
+                    cmbServerAddress.Text = key.GetValue("ServerAddress")?.ToString() ?? string.Empty;
+                    txtUsername.Text = key.GetValue("Username")?.ToString() ?? string.Empty;
+                    chkRememberPassword.Checked = key.GetValue("RememberPassword")?.ToString() == "1";
+
+                    if (chkRememberPassword.Checked)
+                    {
+                        txtPassword.Text = key.GetValue("Password")?.ToString() ?? string.Empty;
+                    }
+
+                    key.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error loading settings: {ex.Message}");
+            }
+        }
+
+        private void FrmConnect_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                Close();
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+
+            if (String.IsNullOrEmpty(cmbServerAddress.Text))
+            {
+                MessageBox.Show("Please enter server address");
+                return;
+            }
+
+            if (String.IsNullOrEmpty(txtUsername.Text))
+            {
+                MessageBox.Show("Please enter username");
+                return;
+            }
+
+            if (String.IsNullOrEmpty(txtPassword.Text))
+            {
+                MessageBox.Show("Please enter password");
+                return;
+            }
+
+            try
+            {
+                RegistryKey key = Registry.CurrentUser.CreateSubKey(RegistryKeyPath);
+                if (key != null)
+                {
+                    key.SetValue("ServerAddress", cmbServerAddress.Text);
+                    key.SetValue("Username", txtUsername.Text);
+                    key.SetValue("RememberPassword", chkRememberPassword.Checked ? "1" : "0");
+
+                    if (chkRememberPassword.Checked)
+                    {
+                        key.SetValue("Password", txtPassword.Text);
+                    }
+                    else
+                    {
+                        key.DeleteValue("Password", false);
+                    }
+
+                    key.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error saving settings: {ex.Message}");
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void txtUsername_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
