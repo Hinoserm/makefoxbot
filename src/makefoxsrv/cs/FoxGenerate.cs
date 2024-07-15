ec8eeeb950f3fc6077ff0d7802425d3265073ff2
Hinoserm <hinoserm@protomuck.org>
2024-07-15 14:11:33 -0500

    Added FoxGenerate.cs, containing useful functions specific to generating images.
    Merged /generate and /img2img into a single command in FoxGenerate.cs.

---
+++ src/makefoxsrv/cs/FoxGenerate.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Reflection;
+using System.IO;
+using System.Reflection.Metadata;
+using System.Linq.Expressions;
+using System.Drawing;
+using MySqlConnector;
+using WTelegram;
+using TL;
+using System.Collections;
+
+// Functions and commands specific to generating images
+
+namespace makefoxsrv
+{
+    internal class FoxGenerate
+    {
+        public static async Task HandleCmdGenerate(FoxTelegram t, Message message, FoxUser user, String? argument, FoxQueue.QueueType imgType = FoxQueue.QueueType.TXT2IMG)
+        {
+            var settings = await FoxUserSettings.GetTelegramSettings(user, t.User, t.Chat);
+
+            if (!string.IsNullOrEmpty(argument))
+            {
+                settings.prompt = argument; //.Replace("\n", ", ");
+                await settings.Save();
+            }
+
+            if (user.DateTermsAccepted is null)
+            {
+                await FoxMessages.SendTerms(t, user, message.ID);
+
+                return; // User must agree to the terms before they can use this command.
+            }
+
+            if (imgType == FoxQueue.QueueType.IMG2IMG)
+            {
+                var loadedImg = await FoxImage.SaveImageFromReply(t, message);
+
+                if (loadedImg is not null)
+                {
+                    settings.selected_image = loadedImg.ID;
+                    await settings.Save();
+                } else if (settings.selected_image > 0) {
+                    loadedImg = await FoxImage.Load(settings.selected_image);
+                }
+
+                if (loadedImg is null)
+                {
+                    if (t.Chat is not null)
+                    {
+                        await t.SendMessageAsync(
+                            text: $"❌ Reply to this message with an image, or send an image with @{FoxTelegram.Client.User.username} in the message, then run /img2img again.",
+                            replyToMessageId: message.ID
+                        );
+                    }
+                    else
+                    {
+                        await t.SendMessageAsync(
+                            text: "❌You must upload or /select an image first to use img2img functions.",
+                            replyToMessageId: message.ID
+                        );
+                    }
+
+                    return;
+                }
+            }
+
+            if (String.IsNullOrEmpty(settings.prompt))
+            {
+                await t.SendMessageAsync(
+                    text: "❌You must specify a prompt!  Please seek /help",
+                    replyToMessageId: message.ID
+                );
+
+                return;
+            }
+
+            long imageSize = settings.width * settings.height;
+            long imageComplexity = imageSize * settings.steps;
+
+            // Default and maximum complexity
+            long defaultComplexity = 640 * 768 * 20;
+            long maxComplexity = 1920 * 1920 * 20;
+
+            double normalizedComplexity = (double)(imageComplexity - defaultComplexity) / (maxComplexity - defaultComplexity);
+
+            int q_limit = 1;
+            switch (user.GetAccessLevel())
+            {
+                case AccessLevel.ADMIN:
+                    q_limit = 20;
+                    break;
+                case AccessLevel.PREMIUM:
+                    if (imageComplexity > 1024 * 1024 * 20)
+                        q_limit = 1;
+                    else
+                        q_limit = 3;
+                    break;
+            }
+
+            // Force reduction of steps if complexity is too high.
+            //if (settings.steps > 20 && imageSize > (1024 * 1024))
+            //    settings.steps = 20;
+
+            if (await FoxQueue.GetCount(user) >= q_limit)
+            {
+                await t.SendMessageAsync(
+                    text: $"❌ Maximum of {q_limit} queued requests per user.",
+                    replyToMessageId: message.ID
+                );
+
+                return;
+            }
+
+            if (await FoxWorker.GetWorkersForModel(settings.model ?? "") is null)
+            {
+                await t.SendMessageAsync(
+                    text: $"❌ There are no workers available to handle your currently selected model ({settings.model}).\r\n\r\nPlease try again later or select a different /model.",
+                    replyToMessageId: message.ID
+                );
+
+                return;
+            }
+
+            if (FoxQueue.CheckWorkerAvailability(settings) is null)
+            {
+                await t.SendMessageAsync(
+                    text: "❌ No workers are available to process this task.\n\nPlease reduce your /size, select a different /model, or try again later.",
+                    replyToMessageId: message.ID
+                );
+
+                return;
+            }
+
+            // Check if the user is premium
+            bool isPremium = user.GetAccessLevel() >= AccessLevel.PREMIUM;
+
+            // Get the total count and recently generated count for the user
+            int totalCount = await FoxQueue.GetTotalCount(user);
+            int recentCount = await FoxQueue.GetRecentCount(user, TimeSpan.FromHours(3));
+
+            TimeSpan? delay = null;
+
+            Message? waitMsg;
+
+            // Apply delay for non-premium users after 100 generations
+            if (!isPremium && totalCount > 100 && normalizedComplexity > 0.001)
+            {
+
+                double complexityDelay = Math.Round(0.3 + normalizedComplexity * (5.0 - 0.3), 1);
+
+                // Calculate additional delay based on recent count
+                double delaySeconds = Math.Round(Math.Min(recentCount * complexityDelay, 60), 1);
+                delay = TimeSpan.FromSeconds(delaySeconds);
+
+                var msgString = $"⏳ Adding to queue...";
+
+                // Nag non-premium users every 5th image or if the delay is substantial
+                if (totalCount % 5 == 0 || delaySeconds > 15)
+                {
+                    msgString += "\n\n✨ Consider a /membership for faster processing and other benefits!";
+                }
+
+                waitMsg = await t.SendMessageAsync(
+                    text: msgString,
+                    replyToMessageId: message.ID
+                );
+
+                FoxLog.WriteLine($"Delaying generation for non-premium user {user.UID} for {delaySeconds:F2} seconds ({complexityDelay}).");
+            }
+            else
+            {
+                //FoxLog.WriteLine($"{message.ID}: CmdGenerate: Checking next position...");
+                (int position, int totalItems) = FoxQueue.GetNextPosition(user, false);
+
+                //FoxLog.WriteLine($"{message.ID}: CmdGenerate: Sending message...");
+                waitMsg = await t.SendMessageAsync(
+                    text: $"⏳ Adding to queue ({position} of {totalItems})...",
+                    replyToMessageId: message.ID
+                );
+            }
+
+            FoxLog.WriteLine($"{message.ID}: CmdGenerate: Calculated complexity: {normalizedComplexity:F3}");
+
+            var q = await FoxQueue.Add(t, user, settings, imgType, waitMsg.ID, message.ID, delay: delay);
+            if (q is null)
+                throw new Exception("Unable to add item to queue");
+
+            await FoxQueue.Enqueue(q);
+        }
+    }
+}
