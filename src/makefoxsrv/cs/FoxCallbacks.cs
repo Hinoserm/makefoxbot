e01dfe35d117ad100d01be6cd6f0884d695e3389
Hinoserm <hinoserm@protomuck.org>
2024-12-09 06:16:57 -0600

    Don't consider variations an "enhanced" queue item.

--- src/makefoxsrv/cs/FoxCallbacks.cs
+++ src/makefoxsrv/cs/FoxCallbacks.cs
@@ -337,7 +337,7 @@ namespace makefoxsrv
                 replyToMessageId: query.msg_id
             );
 
-            var newq = await FoxQueue.Add(t, user, settings, q.Type, waitMsg.ID, query.msg_id, true, q);
+            var newq = await FoxQueue.Add(t, user, settings, q.Type, waitMsg.ID, query.msg_id, false, q);
             if (newq is null)
                 throw new Exception("Unable to add item to queue");
 
