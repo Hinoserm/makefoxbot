9a394eecf378d29e49ba07426fadc39a9d2db8b0
Hinoserm <hinoserm@protomuck.org>
2025-04-18 04:58:31 -0500

    Fix a json thing.

--- src/lib-autofocus/Autofocus/Models/SamplerResponse.cs
+++ src/lib-autofocus/Autofocus/Models/SamplerResponse.cs
@@ -1,4 +1,8 @@
-﻿using System.Text.Json.Serialization;
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace Autofocus.Models;
 
@@ -9,13 +13,67 @@ public interface ISampler
     public IReadOnlyDictionary<string, string> Options { get; }
 }
 
-internal class SamplerResponse
-    : ISampler
+internal class SamplerResponse : ISampler
 {
-    [JsonPropertyName("name")] public string Name { get; init; } = null!;
-    [JsonPropertyName("aliases")] public string[] Aliases { get; init; } = null!;
-    [JsonPropertyName("options")] public Dictionary<string, string> Options { get; init; } = null!;
+    [JsonPropertyName("name")]
+    public string Name { get; init; } = null!;
+
+    [JsonPropertyName("aliases")]
+    public string[] Aliases { get; init; } = null!;
+
+    [JsonPropertyName("options")]
+    [JsonConverter(typeof(DictionaryStringStringConverter))]
+    public Dictionary<string, string> Options { get; init; } = null!;
 
     IReadOnlyList<string> ISampler.Aliases => Aliases;
     IReadOnlyDictionary<string, string> ISampler.Options => Options;
+
+    private class DictionaryStringStringConverter : JsonConverter<Dictionary<string, string>>
+    {
+        public override Dictionary<string, string> Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            var dict = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
+
+            if (reader.TokenType != JsonTokenType.StartObject)
+                throw new JsonException();
+
+            while (reader.Read())
+            {
+                if (reader.TokenType == JsonTokenType.EndObject)
+                    return dict;
+
+                if (reader.TokenType != JsonTokenType.PropertyName)
+                    throw new JsonException();
+
+                string key = reader.GetString()!;
+                reader.Read();
+
+                string value = reader.TokenType switch
+                {
+                    JsonTokenType.String => reader.GetString()!,
+                    JsonTokenType.Number => reader.TryGetInt64(out var i)
+                        ? i.ToString(CultureInfo.InvariantCulture)
+                        : reader.GetDouble().ToString(CultureInfo.InvariantCulture),
+                    JsonTokenType.True => "true",
+                    JsonTokenType.False => "false",
+                    JsonTokenType.Null => "null",
+                    _ => JsonSerializer.Serialize(JsonDocument.ParseValue(ref reader).RootElement)
+                };
+
+                dict[key] = value;
+            }
+
+            throw new JsonException("Unexpected end of JSON.");
+        }
+
+        public override void Write(Utf8JsonWriter writer, Dictionary<string, string> value, JsonSerializerOptions options)
+        {
+            writer.WriteStartObject();
+            foreach (var kvp in value)
+            {
+                writer.WriteString(kvp.Key, kvp.Value);
+            }
+            writer.WriteEndObject();
+        }
+    }
 }
