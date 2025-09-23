1d270b3febd994d4b7e8e3aac6ead1a8b90c4e44
Hinoserm <hinoserm@protomuck.org>
2025-09-23 18:39:22 -0500

    Add debugging message

--- src/makefoxsrv/cs/FoxImage.cs
+++ src/makefoxsrv/cs/FoxImage.cs
@@ -751,7 +751,7 @@ namespace makefoxsrv
                     tags[tag] = prob;
                 }
 
-                //FoxLog.WriteLine($"{this.GetHashCode()}: Loaded {tags.Count} tags for image {this.ID}");
+                FoxLog.WriteLine($"{this.ID}: {this.GetHashCode()}: Loaded {tags.Count} tags for image");
             }
             catch (Exception ex)
             {
@@ -780,7 +780,7 @@ namespace makefoxsrv
             if (_imageTags is null || _imageTags.Count == 0)
                 return; // nothing to save
 
-            //FoxLog.WriteLine($"{this.GetHashCode()}: Saving {_imageTags.Count} tags for image {this.ID}");
+            FoxLog.WriteLine($"{this.ID}: {this.GetHashCode()}: Saving {_imageTags.Count} tags for image");
 
             const int maxRetries = 10;
 
